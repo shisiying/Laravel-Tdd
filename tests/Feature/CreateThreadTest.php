4823cb99b7104bec50e44911add87042cb5ebd9b
shisiying <shisiying@xhzyxed.cn>
2019-07-04 16:49:48 +0800

    feat:参数验证错误

--- tests/Feature/CreateThreadTest.php
+++ tests/Feature/CreateThreadTest.php
@@ -20,12 +20,12 @@ class CreateThreadTest extends TestCase
         // Given we have a signed in user
         $this->signIn();
         // When we hit the endpoint to cteate a new thread
-        $thread = create('App\Thread');
-        $this->post('/threads',$thread->toArray());
+        $thread = make('App\Thread');
+        $response = $this->post('/threads',$thread->toArray());
 
         // Then,when we visit the thread
         // We should see the new thread
-        $this->get($thread->path())
+        $this->get($response->headers->get('Location'))
             ->assertSee($thread->title)
             ->assertSee($thread->body);
     }
@@ -38,11 +38,48 @@ class CreateThreadTest extends TestCase
     {
         $this->withExceptionHandling();
 
-//        $this->get('/threads/create')
-//            ->assertRedirect('/login');
+        $this->get('/threads/create')
+            ->assertRedirect('/login');
 
         $this->post('/threads')
             ->assertRedirect('/login');
     }
 
+    /**
+     * @test
+     */
+    public function a_thread_requires_a_title()
+    {
+       $this->publishThread(['title'=>null])->assertSessionHasErrors('title');
+    }
+
+
+    public function publishThread($overrides = [])
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = make('App\Thread',$overrides);
+
+        return $this->post('/threads',$thread->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function a_thread_requires_a_body()
+    {
+        $this->publishThread(['body'=>null])->assertSessionHasErrors('body');
+    }
+
+    /**
+     * @test
+     */
+    public function a_thread_requires_a_valid_channel()
+    {
+        factory('App\Channel',2)->create();
+
+        $this->publishThread(['channel_id'=>null])->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id'=>999])->assertSessionHasErrors('channel_id');
+    }
 }
